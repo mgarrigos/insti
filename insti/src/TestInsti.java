8eb2c28b2e9f0e56c976e17de19bf0f0e55db831
47327335m <47327335m@katrina-11.infomerce.es>
2015-11-24 19:30:46 +0100

    testpruebas

--- insti/src/TestInsti.java
+++ insti/src/TestInsti.java
@@ -8,29 +8,54 @@ import org.junit.Test;
 
 
 public class TestInsti {
-
+	YearUtilities year=new YearUtilities();
+	
 	public TestInsti() {
 	}
-
+	
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	public static void setUpClass() {
+	System.out.println("Inicio pruebas");
+	System.out.println("-----------------------------------------------------");
 	}
-
+	
 	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
+	public static void tearDownClass() {
+	System.out.println("------------------------------------------------");
+	System.out.println("Fin pruebas");
 	}
-
+	
 	@Before
-	public void setUp() throws Exception {
+	public void setUp()
+	{
+	System.out.println("Comienza el test");
 	}
-
 	@After
-	public void tearDown() throws Exception {
+	public void tearDown()
+	{
+	System.out.println("Termina el test");
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertEquals(3,year.isLeap(1005));	
+	}
+	@Test
+	public void test2() {
+		assertEquals(3,year.isLeap(-2000));	
+	}
+	@Test
+	public void test3() {
+		assertEquals(-2,year.isLeap(0));	
+	}
+	@Test
+	public void test4() {
+		assertEquals(3,year.isLeap(4000));	
 	}
 
+
 }
+
+	
+
+
