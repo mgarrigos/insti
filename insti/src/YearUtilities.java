3b5159ed479738b86057c032e104ad315afda2e2
47327335m <47327335m@katrina-11.infomerce.es>
2015-11-24 19:53:28 +0100

    final

--- insti/src/YearUtilities.java
+++ insti/src/YearUtilities.java
@@ -7,7 +7,7 @@ public class YearUtilities {
 	                if(year%400 == 0){
 	                    return true; 
 	                }else{
-	                    return true;  
+	                    return false;  
 	                }
 	            }else{
 	                return true;
